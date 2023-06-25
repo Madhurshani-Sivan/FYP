0fcdff4ea6a02a4dd2884770556e548ec4aebe04
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-06-25 12:59:17 +0530

    api fo eligible courses

--- db.py
+++ db.py
@@ -1,5 +1,12 @@
+import json
+from bson import ObjectId
 import pymongo
 from pymongo import MongoClient
+import pandas as pd
+from functions import convert_float_values
+from fastapi.responses import JSONResponse
+import numpy as np
+
 
 def get_courselist_collection():
     uri = 'mongodb+srv://fyp:fyp123@courselist.i0n97yv.mongodb.net/?retryWrites=true&w=majority'
@@ -14,3 +21,93 @@ def get_uni_location_collection():
     db = client.courselist
     collection = db.uni_location
     return collection
+
+def get_all_courses_collection():
+    courselist_collection = get_courselist_collection()
+    universities_collection = get_uni_location_collection()
+
+    courses = courselist_collection.find({}, {'code': 1, 'course': 1, 'uni': 1})
+    courses = pd.DataFrame(courses)
+    courses.drop('_id', inplace=True, axis=1)
+
+    universities = universities_collection.find({}, {'code': 1, 'name': 1})
+    universities = pd.DataFrame(universities)
+    universities.drop('_id', inplace=True, axis=1)
+
+    combined_dataframes = []
+
+    for _, row in courses.iterrows():
+        code = str(row['code'])
+        uni = row['uni']
+        code = code.zfill(3)
+        df = pd.DataFrame({'code': [code + val for val in uni], 'course': row['course']})
+        df['university'] = df['code'].str[-1].map(universities.set_index('code')['name'])
+        combined_dataframes.append(df)
+
+    all_courses = pd.concat(combined_dataframes, ignore_index=True)
+    all_courses = all_courses.sort_values('code').drop_duplicates(subset=['code']).reset_index(drop=True)
+
+    all_courses['code'] = all_courses['code'].astype(str)
+    all_courses['course'] = all_courses['course'].astype(str)
+    all_courses['university'] = all_courses['university'].astype(str)
+
+    return all_courses.to_dict(orient='records')
+
+def get_eligible_courses_collection(student):
+    student_stream = student.get("stream")
+
+    ol_results = student.get("ol")
+    english_grade = ol_results.get("English")
+    maths_grade = ol_results.get("Maths")
+    science_grade = ol_results.get("Science")
+
+    sub1 = student.get("al")[0]
+    sub2 = student.get("al")[1]
+    sub3 = student.get("al")[2]
+
+    sub1_subject = sub1.get("sub")
+    sub1_results = sub1.get("results")
+
+    sub2_subject = sub2.get("sub")
+    sub2_results = sub2.get("results")
+
+    sub3_subject = sub3.get("sub")
+    sub3_results = sub3.get("results")
+
+
+    courselist_collection = get_courselist_collection()
+
+    results_after_ol = courselist_collection.find({"$or": [{"stream": student_stream},{"stream": "Extra"}], \
+                                         "$and":[
+                                        {"$and":[{ "$or":[{"ol.English":{"$gte":english_grade}},{"ol.English":""}]}, \
+                                         {"$or":[{"ol.Maths":{"$gte":maths_grade}},{"ol.Maths":""}]}, \
+                                         {"$or":[{"ol.Science":{"$gte":science_grade}},{"ol.Science":""}]}]},\
+                                         {"$or":[{"al":[]}, \
+                                                 {"$or":[{"al":{"$elemMatch":{sub1_subject:{"$gte":sub1_results}}}},\
+                                                             {"al":{"$elemMatch":{sub2_subject:{"$gte":sub2_results}}}},\
+                                                  {"al":{"$elemMatch":{sub3_subject:{"$gte":sub3_results}}}},\
+                                                  ]}]}, \
+                                        {"$or":[{"30-21":{"$exists": False}}, \
+                                                 {"$or":[{"30-21":{"$elemMatch":{sub1_subject:{"$gte":sub1_results}}}},\
+                                                             {"30-21":{"$elemMatch":{sub2_subject:{"$gte":sub2_results}}}},\
+                                                  {"30-21":{"$elemMatch":{sub3_subject:{"$gte":sub3_results}}}},\
+                                                  ]}]}, \
+                                        {"$or":[{"21-12":{"$exists": False}}, \
+                                                 {"$or":[{"21-12":{"$elemMatch":{sub1_subject:{"$gte":sub1_results}}}},\
+                                                             {"21-12":{"$elemMatch":{sub2_subject:{"$gte":sub2_results}}}},\
+                                                  {"21-12":{"$elemMatch":{sub3_subject:{"$gte":sub3_results}}}},\
+                                                  ]}]}, \
+                                        {"$or":[{"30-21-12":{"$exists": False}}, \
+                                                 {"$or":[{"30-21-12":{"$elemMatch":{sub1_subject:{"$gte":sub1_results}}}},\
+                                                             {"30-21-12":{"$elemMatch":{sub2_subject:{"$gte":sub2_results}}}},\
+                                                  {"30-21-12":{"$elemMatch":{sub3_subject:{"$gte":sub3_results}}}},\
+                                                  ]}]}]}, \
+                                        {"code":1,"course":1,"uni":1,"al":1,"30-21":1,"21-12":1,"30-21-12":1})
+    
+    results_after_ol = pd.DataFrame(results_after_ol)
+    results_after_ol.drop('_id', inplace=True, axis=1)
+    results_after_ol = results_after_ol.astype(object)
+    results_after_ol = results_after_ol.applymap(lambda x: int(x) if isinstance(x, np.int64) else x)
+    json_data = results_after_ol.to_json(orient='records')
+
+    return json_data
--- routes.py
+++ routes.py
@@ -1,6 +1,10 @@
+import ast
+import json
 from fastapi import APIRouter
-from db import get_courselist_collection, get_uni_location_collection
+from db import get_courselist_collection, get_uni_location_collection, get_all_courses_collection, get_eligible_courses_collection
 import pandas as pd
+from functions import al, ThreeZero_TwoOne, TwoOne_OneTwo, ThreeZero_TwoOne_OneTwo
+
 
 router = APIRouter()
 
@@ -29,32 +33,76 @@ def get_all_universities():
 
 @router.get('/all_courses')
 def get_all_courses():
-    courselist_collection = get_courselist_collection()
+    return get_all_courses_collection()
+
+@router.get('/eligible_courses')
+def get_eligible_courses():
+    student = {
+        "stream":"Commerce",
+        "ol": {
+            "English":"A",
+            "Maths":"A",
+            "Science":"A"
+        },
+        "al": [
+            {
+                "sub": "Economics",
+                "results":"A"
+            },
+            {
+                "sub": "Accounting",
+                "results":"A"
+            },
+            {
+                "sub": "ICT",
+                "results":"A"
+            }
+        ]
+    }
+    courses = get_eligible_courses_collection(student)
+
+    sub1 = student.get("al")[0].get("sub")
+    sub2 = student.get("al")[1].get("sub")
+    sub3 = student.get("al")[2].get("sub")
+    subs=[sub1,sub2,sub3]
+
+    courses = json.loads(courses)
+
+    results_after_al = json.loads(al(courses, subs))
+    results_after_TZ_TO = json.loads(ThreeZero_TwoOne(results_after_al, subs))
+    results_after_TO_OT = json.loads(TwoOne_OneTwo(results_after_TZ_TO, subs))
+    results_after_TZ_TO_OT = json.loads(ThreeZero_TwoOne_OneTwo(results_after_TO_OT, subs))
+
     universities_collection = get_uni_location_collection()
-
-    courses = courselist_collection.find({}, {'code': 1, 'course': 1, 'uni': 1})
-    courses = pd.DataFrame(courses)
-    courses.drop('_id', inplace=True, axis=1)
-
     universities = universities_collection.find({}, {'code': 1, 'name': 1})
     universities = pd.DataFrame(universities)
     universities.drop('_id', inplace=True, axis=1)
 
     combined_dataframes = []
 
-    for _, row in courses.iterrows():
-        code = str(row['code']) 
-        uni = row['uni']  
+    eligible_courses = pd.DataFrame(results_after_TZ_TO_OT)
+
+    for _, row in eligible_courses.iterrows():
+        code = str(row['code'])
+        uni = row['uni']
         code = code.zfill(3)
         df = pd.DataFrame({'code': [code + val for val in uni], 'course': row['course']})
         df['university'] = df['code'].str[-1].map(universities.set_index('code')['name'])
         combined_dataframes.append(df)
 
-    all_courses = pd.concat(combined_dataframes, ignore_index=True)
-    all_courses = all_courses.sort_values('code').drop_duplicates(subset=['code']).reset_index(drop=True)
+    eligible_courses = pd.concat(combined_dataframes, ignore_index=True)
+    eligible_courses = eligible_courses.sort_values('code').drop_duplicates(subset=['code']).reset_index(drop=True)
 
-    all_courses['code'] = all_courses['code'].astype(str)
-    all_courses['course'] = all_courses['course'].astype(str)
-    all_courses['university'] = all_courses['university'].astype(str)
+    eligible_courses['code'] = eligible_courses['code'].astype(str)
+    eligible_courses['course'] = eligible_courses['course'].astype(str)
+    eligible_courses['university'] = eligible_courses['university'].astype(str)
+
+    return eligible_courses.to_dict(orient='records')
+
+    #df_courses = pd.DataFrame(results_after_TZ_TO_OT)
+    #df_courses.drop('_id', inplace=True, axis=1)
+    #df_courses.drop('al', inplace=True, axis=1)
+    #df_courses.drop('30-21', inplace=True, axis=1, errors='ignore')
+    #df_courses.drop('21-12', inplace=True, axis=1, errors='ignore')
     
-    return all_courses.to_dict(orient='records')
+
